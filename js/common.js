6ae9d0a5d6d61c5a687fb33e55506762b36bdbc3
Maksym Marko <maximus21@mail.ru>
2016-03-01 14:32:17 +0200

    It works when you click on the word input

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	
+		
+	$(".SpCklick").click(function() {
+		if($(".DialogWind").is(":visible")){
+			 $(".DialogWind").hide(0);
+			 $(".DialTriangle").hide(0);
+		}
+	     	else{
+		 	 $(".DialogWind").show(0);
+			 $(".DialTriangle").show(0);
+		 } 	     
+	});
+	   
+	$("header").click(function(){
+	   		$(".DialogWind").hide(0);
+			$(".DialTriangle").hide(0);
+	});
 })
